33abdf6bd42c51218c1f03da6a58ce6bff55d59c
Yuma Uchiumi <yumaloop@gmail.com>
2020-11-23 17:37:48 +0900

    update

--- data/main.py
+++ data/main.py
@@ -1,29 +1,25 @@
 import sys
 sys.path.append("..")
-
 import numpy as np
 import pandas as pd
 import yfinance as yf
 import pandas_datareader
-
 from tqdm import tqdm
 from datetime import datetime
 from pprint import pprint
 from models import MarkowitzMinVarianceModel
 
-
+# all symbols in TOPIX 500 on 2011/10/31
 df_tpx500 = pd.read_csv("../data/master/universe_tse_topix500_20011031-20201031.csv")
 s = df_tpx500["20111031"].values
 s = s[~np.isnan(s)] # delete nan
 symbols_tpx500 = s.astype(np.int64).astype(str) # cast as str type
 symbols_tpx500 = [str(s)+'.T' for s in symbols_tpx500]
 
-st = '2011/10/31' 
-ed = '2020/10/31' 
 
+# df: time-series price data of stocks in `symbols_tpx500`
 st = datetime(2011, 10, 31)
 ed = datetime(2020, 10, 31)
-
 dfs = []
 for symbol in tqdm(symbols_tpx500):
     try:
@@ -35,6 +31,23 @@ for symbol in tqdm(symbols_tpx500):
         dfs.append(df)
     except:
         pass
-    
 df = pd.concat(dfs, axis=1)
-df.to_csv("../data/tse_tpx500_20111031-202010131.csv")
+
+# fill nan
+for col in df.columns:
+    st_idx = df[col].first_valid_index()
+    ed_idx = df[col].last_valid_index()
+
+    # for any columns (=stocks)
+    if df[col].isnull().any():
+        # 新規上場
+        if st_idx != df.index[0]:
+            df[col] = df[col].fillna(df[col][st_idx])
+
+        # 上場廃止
+        if df.index[-1] != ed_idx:
+            df[col] = df[col].fillna(df[col][ed_idx])
+
+# save df as .csv file
+if not df.isnull().any()
+    df.to_csv("./tse_tpx500_20111031-202010131.csv", index=True)
